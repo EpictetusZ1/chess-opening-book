ee40f3e409af86675b2bdafbd253a0222bc92159
J Heaton <jackrobertheaton@gmail.com>
2022-03-31 12:17:30 -0400

    Add RegEx to parse game moves and tags

--- server/utils/gameController.js
+++ server/utils/gameController.js
@@ -1,13 +1,25 @@
 const fs = require('fs')
 const path = require("path");
-
 exports.fileUpload = (req, res, next) => {
-
     fs.readFile(path.resolve(__dirname, `../${req.file.path}`), 'utf8' , (err, data) => {
         if (err) {
             console.error(err)
             return
         }
+
+        const cleanGame = async () => {
+            const patternTags = /\[.*]/gm
+            const tags = data.match(patternTags)
+
+            const movePattern = /(?<moves>[0-9]+\.\s(?<plys>[Oo]-[Oo]-[Oo]|[Oo]-[Oo]\s?|[KQBNR]?x?\+?[a-h]?[1-8]?x?\+?\s?[KQBNR]?x?[a-h]x?[a-h]?[1-8]\s?x?[+#]?\s?(\s?[0-9]-[0-9])?)*)/gm
+
+            const moveArr = [...data.matchAll(movePattern)]
+                .map((item) => {
+                    return item.groups.moves.replace(/\r?\n|\r/gm, " ")
+                })
+        }
+        cleanGame()
+
         res.send(data)
     })
 }
