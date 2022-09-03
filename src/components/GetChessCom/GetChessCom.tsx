d9a6c26a068b0b825071840abd1b560322f1671c
Jack Heaton <jackrobertheaton@gmail.com>
2022-09-03 18:18:05 -0400

    Add RegEx to remove inline clock values, complete chessCOM integration api logic

--- src/components/GetChessCom/GetChessCom.tsx
+++ src/components/GetChessCom/GetChessCom.tsx
@@ -2,9 +2,13 @@ import * as S from "./GetChessCom.styles"
 import React, {ReactEventHandler, useState} from "react";
 import axios from "axios";
 import {fakeData} from "./fakeData";
+import {handleFileUpload} from "../../utils/parseGame";
+import {useSession} from "next-auth/react";
 
 const GetChessCom = () => {
+    const { data: session, status } = useSession()
     const [username, setUsername] = useState("")
+    console.log("session", session)
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.value !== "") {
@@ -16,12 +20,9 @@ const GetChessCom = () => {
         e.preventDefault()
         // const res = await axios.get(`/api/chessCom/${username}`)
         const gameArr = fakeData.games
-
-        gameArr.forEach((game) => {
-            console.log("Game ------------------")
-            console.log( game.pgn)
-
-        })
+        const res = await axios.post(`/api/game/add/${session?.user?.id}`,
+            { data: gameArr[0].pgn, provider: "chessCom" }
+        )
     }
 
     return (
