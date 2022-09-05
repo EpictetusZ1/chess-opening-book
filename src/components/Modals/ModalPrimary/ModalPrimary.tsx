f45b99679392bc0084098cc11428ac44a765010d
Jack Heaton <jackrobertheaton@gmail.com>
2022-09-05 13:20:35 -0400

    Add Primary modal component

---
+++ src/components/Modals/ModalPrimary/ModalPrimary.tsx
@@ -0,0 +1,13 @@
+import * as S from "./ModalPrimary.styles";
+
+const ModalPrimary = () => {
+    return (
+        <S.ModalPrimaryCont>
+            <S.ModalPrimary>
+
+            </S.ModalPrimary>
+        </S.ModalPrimaryCont>
+    );
+};
+
+export default ModalPrimary;
