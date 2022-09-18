9a8bb94be904815bce878dee6aef81113aadfe93
Jack Heaton <jackrobertheaton@gmail.com>
2022-09-18 18:23:27 -0400

    Basic non-functional opening table

--- src/components/OpeningExplorer/OpeningExplorer.styles.ts
+++ src/components/OpeningExplorer/OpeningExplorer.styles.ts
@@ -1,7 +1,57 @@
 import styled from 'styled-components';
 
 
-export const OpeningExIsland = styled.div`
-    width: 25%;
-    height: 100vh;
+export const OpeningExIsland = styled.table`
+  width: 25%;
+  border-spacing: 0 2px;
+`
+
+export const MoveList = styled.tbody`
+  width: 100%;
+  height: auto;
+  
+  tr {
+    width: 100%;
+    margin: auto 0;
+  }
+  
+  td {
+    padding: 5px 0;
+  }
+  
+  tr:nth-of-type(odd) {
+    background-color: rgba(94, 94, 94, 0.35);
+  }
+   
+  .moveValue {
+    width: 30%;
+    text-align: center;
+  }
+  
+  .moveFreq {
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+`
+
+type Props = {
+    width: number;
+}
+
+export const MoveBar = styled.span<Props>`
+  width: ${(props) => `${props.width}%`};
+  min-width: 20px;
+  background-color: #000;
+  opacity: 1;
+  border-radius: 1rem 0 0 1rem;
+  padding-right:  ${(props) => `${(props.width) + 10}%`};
+  
+  .moveFreqText {
+    padding: 0 10px;
+    text-align: left;
+  }
 `
--- src/components/OpeningExplorer/OpeningExplorer.tsx
+++ src/components/OpeningExplorer/OpeningExplorer.tsx
@@ -1,63 +1,116 @@
 import * as S from "./OpeningExplorer.styles";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {IGame} from "../../types/Game.types";
+import CreateOpeningMatrix from "../../utils/createOpeningMatrix";
+import {StyledComponent, StyledComponentBase, StyledInterface} from "styled-components";
 
 
 type Props = {
     gameData: IGame[]
 }
 
+
 const OpeningExplorer = ({gameData}: Props) => {
-    const [moveList, setMoveList] = useState<string[]>([""])
-    const [potentialVariations, setPotentialVariations] = useState<any[]>([])
+    type TNextMove = {
+        id: string
+        freq: number
+        prevMove: string
+        variations: [TNextMove]
+    }
 
-    //TODO: Fix round parsing
+    type TExplorerData = {
+        possibleNextMoves: TNextMove[]
+        currIndex: number
+    }
 
+    const initialExplorerData: TExplorerData = {
+        possibleNextMoves: [],
+        currIndex: 0
+    }
+    // const [moveList, setMoveList] = useState<string[]>([""])
+    // const [nextMoves, setNextMoves] = useState<any[]>([])
+
+    const [explorerData, setExplorerData] = useState<any>(initialExplorerData)
+    const [showData, setShowData] = useState<boolean>(false)
 
     useEffect(() => {
-        const data = {
-            startIndex: 0,
-            moveList: ["e4", "c5"]
-        }
+        // const getMoveList = async () => {
+        //     const res = await axios.post(`/api/game/byMoves`, data)
+        //     if (res.data) {
+        //         console.log("Res from game by moves: ", res.data.result)
+        //         setMoveList(res.data.result)
+        //     }
+        // }
+        // getMoveList()
 
-        const data2 = {
-            startIndex: 0,
-            // moveList: ["e4", "c5", "Nc3", "Nc6", "Nf3", "e6", "d4", "cxd4", "Nxd4", "a6", "Nxc6", "bxc6", "Bd3", "Be7", "O-O", "O-O", "Qe2", "d6", "Rfd1", "Qc7", "Qg4", "Rac8", "Qxg7", "Rxc2", "Qh6+", "Kf8", "Qxh7", "Rc1+", "Kg2"]
-            moveList: ["e4", "c5", "Nc3", "Nc6", "Nf3", "e6",]
-
-        }
-
-        const getMoveList = async () => {
-            const res = await axios.post(`/api/game/byMoves`, data2)
-            // const res = await axios.post(`/api/opening`, data)
-            if (res.data) {
-                console.log("Res from game by moves: ", res.data.result)
-                setMoveList(res.data.result)
-            }
-        }
-        getMoveList()
+        //@ts-ignore
+        const result = CreateOpeningMatrix(gameData)
+        let target = parseInt(explorerData.currIndex)
+        setExplorerData({
+            ...explorerData,
+            possibleNextMoves: result[target]
+        })
+        setShowData(true)
 
     }, [])
 
-
-
     return (
         <S.OpeningExIsland>
+            <thead>
+            <tr>
+                <th>Move</th>
+                <th>Frequency</th>
+            </tr>
+            </thead>
+            <S.MoveList>
+                { showData && explorerData.possibleNextMoves.map((move: any, index: number) => {
+                    return (
+                        <tr key={index} className={"move"}>
+                            <td className={"moveValue"}>
+                                {move.id}
+
+                            </td>
+                            <td className={"moveFreq"}>
+                                <S.MoveBar width={Math.floor((move.freq / gameData.length) * 100)}>
+                                    <p className="moveFreqText">
+                                        {move.freq}
+                                    </p>
+                                </S.MoveBar>
+                            </td>
+                        </tr>
+                    )
+                })}
+            </S.MoveList>
+
+
+            {/*            <thead>
+            <tr>
+                <th>Players</th>
+                <th>Result</th>
+                <th>Moves</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {games.map((game: IGame) => (
+                <tr key={game.id}>
+                    <td>
+                        <div className={"playerInfo"}>
+                            <span><b>{game.white}</b> ({game.whiteElo})</span>
+                            <span><b>{game.black}</b> ({game.blackElo})</span>
+                        </div>
+                    </td>
+                    <td>{game.result}</td>
+                    <td>{Math.floor(game.moves.length / 2)}</td>
+                    <td>{game.date}</td>
+                </tr>
+            ))}
+            </tbody>*/}
+
 
         </S.OpeningExIsland>
     );
 };
 
 export default OpeningExplorer;
-
-// {potentialVariations.map((variation, i) => {
-//     return (
-//         <div key={variation + i} className={"variationItem"}>
-//             <p>{variation.move} </p>
-//             &nbsp;
-//             <p>{variation.freq}</p>
-//             {/*<p>{variation.variations}</p>*/}
-//         </div>
-//     )
-// })}
